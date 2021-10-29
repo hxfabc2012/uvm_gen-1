7752c0d915c20116bb68a2513abecc2437536235
dpoulin <dpoulin@localhost>
2021-10-29 14:55:25 -0400

    sanity_misc passing

--- tools/uvm_gen/bin/sanity_misc.sh
+++ tools/uvm_gen/bin/sanity_misc.sh
@@ -13,23 +13,24 @@
 
 
 # 1. Create code from spec files
-#cat ./tests/lib.spec         | ./src/new_library.py ../../dv/
-cat ./tests/component.spec   | ./src/new.py         ../../dv/uvmt_misc_st/src/tests
-#cat ./tests/object.spec      | ./src/new.py         ../../dv/uvmt_misc_st/src/tests
-#cat ./tests/reg_adapter.spec | ./src/new.py         ../../dv/uvmt_misc_st/src/tests
-#cat ./tests/reg.spec         | ./src/new.py         ../../dv/uvmt_misc_st/src/tests
-#cat ./tests/reg_block.spec   | ./src/new.py         ../../dv/uvmt_misc_st/src/tests
-#cat ./tests/seq.spec         | ./src/new.py         ../../dv/uvmt_misc_st/src/seq
-#cat ./tests/seq_lib.spec     | ./src/new.py         ../../dv/uvmt_misc_st/src/seq
-#cat ./tests/test.spec        | ./src/new.py         ../../dv/uvmt_misc_st/src/tests
-#cat ./tests/vseq.spec        | ./src/new.py         ../../dv/uvmt_misc_st/src/seq
-#cat ./tests/vseq_lib.spec    | ./src/new.py         ../../dv/uvmt_misc_st/src/seq
+cat ./tests/lib.spec | ./src/new_lib.py ../../dv/
+./src/new.py component   -f ./tests/component.spec   -o ../../dv/uvmt_misc_st/src/tests
+./src/new.py object      -f ./tests/object.spec      -o ../../dv/uvmt_misc_st/src/tests
+./src/new.py reg_adapter -f ./tests/reg_adapter.spec -o ../../dv/uvmt_misc_st/src/tests
+./src/new.py reg         -f ./tests/reg.spec         -o ../../dv/uvmt_misc_st/src/tests
+./src/new.py reg_block   -f ./tests/reg_block.spec   -o ../../dv/uvmt_misc_st/src/tests
+./src/new.py seq         -f ./tests/seq.spec         -o ../../dv/uvmt_misc_st/src/seq
+./src/new.py seq_lib     -f ./tests/seq_lib.spec     -o ../../dv/uvmt_misc_st/src/seq
+./src/new.py test        -f ./tests/test.spec        -o ../../dv/uvmt_misc_st/src/tests
+./src/new.py vseq        -f ./tests/vseq.spec        -o ../../dv/uvmt_misc_st/src/seq
+./src/new.py vseq_lib    -f ./tests/vseq_lib.spec    -o ../../dv/uvmt_misc_st/src/seq
 
 # 2. Simulate
-#cd ../../sim
-#./setup_project.py
-#source ./setup_terminal.sh
-#dvm all uvmt_misc_st -t smoke -s 1
+cd ../../sim
+./setup_project.py
+source ./setup_terminal.sh
+../tools/.imports/dvm/src/dvm.py cpel uvmt_misc_st
+../tools/.imports/dvm/src/dvm.py sim uvmt_misc_st -t smoke -s 1
 
 # 3. Gather sim results
-#../tools/dvm/src/dvm.py results uvmt_misc_st misc
+../tools/.imports/dvm/src/dvm.py results uvmt_misc_st misc
