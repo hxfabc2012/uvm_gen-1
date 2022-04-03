9133e3907e9d9d7d5ad9ba0a1489c2b32dc8a7e5
datum-dpoulin <dpoulin@datumtc.ca>
2022-04-03 14:25:00 -0400

    Small improvements to lib template

--- tools/uvm_gen/templates/lib/src/tdefs.sv
+++ tools/uvm_gen/templates/lib/src/tdefs.sv
@@ -3,12 +3,19 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
 `ifndef __UVML_${name_uppercase}_TDEFS_SV__
 `define __UVML_${name_uppercase}_TDEFS_SV__
 
 
-
+// Add enums and structs here
+// Ex: typedef bit [(`UVML_${name_uppercase}_ABC_MAX_WIDTH-1):0]  uvml_${name}_abc_b_t;
+// Ex: typedef enum {
+//        UVML_${name_uppercase}_MY_ABC
+//     } uvml_${name}_my_enum;
+// Ex: typedef struct {
+//        bit [2:0]  abc;
+//        logic      xyz;
+//     } uvml_${name}_my_struct;
 
 
 `endif // __UVML_${name_uppercase}_TDEFS_SV__
