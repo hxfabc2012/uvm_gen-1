b117c9464e9848fcb585f55c5ad697e39b6d88a8
datum-dpoulin <dpoulin@datumtc.ca>
2022-07-16 01:19:42 -0400

    removing final block from misc tb

--- dv/uvmt_misc_st/src/tb/uvmt_misc_st_tb.sv
+++ dv/uvmt_misc_st/src/tb/uvmt_misc_st_tb.sv
@@ -18,14 +18,14 @@
  * Module encapsulating the Miscellaneous Self-Test DUT wrapper, agents and clock generating interfaces.
  */
 module uvmt_misc_st_tb;
-   
+
    import uvm_pkg::*;
    import uvmt_misc_st_pkg::*;
-   
+
    // Clocking & Reset
    uvmt_misc_st_clknrst_gen_if  clknrst_gen_if();
-   
-   
+
+
    /**
     * Test bench entry point.
     */
@@ -35,75 +35,18 @@ module uvmt_misc_st_tb;
          .units_number       (   -9),
          .precision_number   (    3),
          .suffix_string      (" ns"),
-         .minimum_field_width(   18) 
+         .minimum_field_width(   18)
       );
-      
+
       // Add interfaces to uvm_config_db
       uvm_config_db#(virtual uvmt_misc_st_clknrst_gen_if)::set(null, "*", "clknrst_gen_vif", clknrst_gen_if);
-      
+
       // Run test
       uvm_top.enable_print_topology = 0;
       uvm_top.finish_on_completion  = 1;
       uvm_top.run_test();
    end
-   
-   
-   /**
-    * End-of-test summary printout.
-    */
-   final begin
-      string             summary_string;
-      uvm_report_server  rs;
-      int                err_count;
-      int                fatal_count;
-      static bit         sim_finished = 0;
-      
-      static string  red   = "\033[31m\033[1m";
-      static string  green = "\033[32m\033[1m";
-      static string  reset = "\033[0m";
-      
-      rs          = uvm_top.get_report_server();
-      err_count   = rs.get_severity_count(UVM_ERROR);
-      fatal_count = rs.get_severity_count(UVM_FATAL);
-      
-      void'(uvm_config_db#(bit)::get(null, "", "sim_finished", sim_finished));
-      
-      $display("\n*** Test Summary ***\n");
-      
-      if (sim_finished && (err_count == 0) && (fatal_count == 0)) begin
-         $display("    PPPPPPP    AAAAAA    SSSSSS    SSSSSS   EEEEEEEE  DDDDDDD     ");
-         $display("    PP    PP  AA    AA  SS    SS  SS    SS  EE        DD    DD    ");
-         $display("    PP    PP  AA    AA  SS        SS        EE        DD    DD    ");
-         $display("    PPPPPPP   AAAAAAAA   SSSSSS    SSSSSS   EEEEE     DD    DD    ");
-         $display("    PP        AA    AA        SS        SS  EE        DD    DD    ");
-         $display("    PP        AA    AA  SS    SS  SS    SS  EE        DD    DD    ");
-         $display("    PP        AA    AA   SSSSSS    SSSSSS   EEEEEEEE  DDDDDDD     ");
-         $display("    ----------------------------------------------------------");
-         $display("                        SIMULATION PASSED                     ");
-         $display("    ----------------------------------------------------------");
-      end
-      else begin
-         $display("    FFFFFFFF   AAAAAA   IIIIII  LL        EEEEEEEE  DDDDDDD       ");
-         $display("    FF        AA    AA    II    LL        EE        DD    DD      ");
-         $display("    FF        AA    AA    II    LL        EE        DD    DD      ");
-         $display("    FFFFF     AAAAAAAA    II    LL        EEEEE     DD    DD      ");
-         $display("    FF        AA    AA    II    LL        EE        DD    DD      ");
-         $display("    FF        AA    AA    II    LL        EE        DD    DD      ");
-         $display("    FF        AA    AA  IIIIII  LLLLLLLL  EEEEEEEE  DDDDDDD       ");
-         
-         if (sim_finished == 0) begin
-            $display("    --------------------------------------------------------");
-            $display("                   SIMULATION FAILED - ABORTED              ");
-            $display("    --------------------------------------------------------");
-         end
-         else begin
-            $display("    --------------------------------------------------------");
-            $display("                       SIMULATION FAILED                    ");
-            $display("    --------------------------------------------------------");
-         end
-      end
-   end
-   
+
 endmodule : uvmt_misc_st_tb
 
 
